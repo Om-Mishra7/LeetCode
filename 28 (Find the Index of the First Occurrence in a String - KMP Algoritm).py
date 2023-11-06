51d72d60b1da1e81c896c0a1be8175e39f07dfce
Om Mishra <admin@projectrexa.dedyn.io>
2023-11-06 11:56:52 +0530

    Update 28 (Find the Index of the First Occurrence in a String - KMP Algoritm).py

--- 28 (Find the Index of the First Occurrence in a String - KMP Algoritm).py
+++ 28 (Find the Index of the First Occurrence in a String - KMP Algoritm).py
@@ -1,9 +1,9 @@
 def strStr(haystack, needle):
+    if not needle:
+        return 0
 
     # Preprocessing for LPS
-
     lps = [0] * len(needle)
-
     previous_lps_pointer, current_lps_pointer = 0, 1
 
     while current_lps_pointer < len(needle):
@@ -11,34 +11,30 @@ def strStr(haystack, needle):
             lps[current_lps_pointer] = previous_lps_pointer + 1
             previous_lps_pointer += 1
             current_lps_pointer += 1
-        elif lps[previous_lps_pointer] == 0:
+        elif previous_lps_pointer == 0:
             lps[current_lps_pointer] = 0
             current_lps_pointer += 1
         else:
             previous_lps_pointer = lps[previous_lps_pointer - 1]
 
-
     # Searching the haystack for the needle
-
-    needle_pointer, haystack_pointer  = 0, 0    
+    needle_pointer, haystack_pointer = 0, 0
 
     while haystack_pointer < len(haystack):
         if haystack[haystack_pointer] == needle[needle_pointer]:
             haystack_pointer += 1
             needle_pointer += 1
+            if needle_pointer == len(needle):
+                return haystack_pointer - len(needle)
         else:
             if needle_pointer == 0:
                 haystack_pointer += 1
             else:
-                needle_pointer = lps[needle_pointer-1]
-        if needle_pointer == len(needle):
-            return haystack_pointer - len(needle)
-    
+                needle_pointer = lps[needle_pointer - 1]
+
     return -1
-    
 
-
-haystack = "mississippi"
-needle = "issip"
+haystack = "aabaaabaaac"
+needle = "aabaaac"
 
 print(strStr(haystack, needle))
